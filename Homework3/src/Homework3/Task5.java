39c9f869ebfc9731eb42843bd9b27b262929099b
Egorych <mr.stavr@mail.ru>
2016-07-09 13:39:14 +0200

    Homework3

--- Homework3/src/Homework3/Task5.java
+++ Homework3/src/Homework3/Task5.java
@@ -3,16 +3,18 @@ package Homework3;
 public class Task5 {
 
 	public static void main(String[] args) {
-		String s = (" hjghjghjghj g  hjh");
+		String s = ("cfdghjhgfdslf  dsf ");
 		char[] sToArray = s.toCharArray();
 		int count = 0;
-		for(int i = 0; i <= sToArray.length; i++)
+		outerloop:
+		for(int i = 0; i < sToArray.length; i++)
 		{
-			for(int j = i+1; j <= sToArray.length-1;j++)
+			for(int j = i+1; j < sToArray.length;j++)
 			{
 				if(sToArray[i]==sToArray[j])
 				{
 					count++;
+					break outerloop;
 				}
 			}
 		}
