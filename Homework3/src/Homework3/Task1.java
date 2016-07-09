f7be0dd7cd1b89b63b43d0c46631e536e88cbdd6
Egorych <mr.stavr@mail.ru>
2016-07-10 00:42:50 +0200

    Homework3

--- Homework3/src/Homework3/Task1.java
+++ Homework3/src/Homework3/Task1.java
@@ -3,20 +3,20 @@ package Homework3;
 public class Task1 {
 
 	public static void main(String[] args) {
-		String text = new String("Стояла суровая зима. Все было покрыто снегом. Тяжело пришлось от этого воробушкам. Бедняжки нигде не могли найти корма.");
-		String[] words = text.split(" ");
-		int countWords = words.length;
-		int k = 5;
-		for (int i = 0; i < countWords; i++)
+		String text = new String("Стояла суровая зима. Все было покрыто снегом. Тяжело пришлось от этого воробушкам. Бедняжки нигде не могли найти корма.");//create the line of type String
+		String[] words = text.split(" ");//break a line into words
+		int countWords = words.length;//create a variable of type int
+		int k = 5;//appropriate value to a variable 
+		for (int i = 0; i < countWords; i++)//cycle
 		{
-			if (words[i].length() > k)
+			if (words[i].length() > k) 
 			{
-				char[] wordToCharArray = words[i].toCharArray();
-				wordToCharArray[k] = '!';
-				words[i] = new String(wordToCharArray);
+				char[] wordToCharArray = words[i].toCharArray();//transfer the word to a line of symbols
+				wordToCharArray[k] = '!';//give to an k element the set symbol
+				words[i] = new String(wordToCharArray);//write down value to the massif
 				
 			}
-			System.out.print (words[i] + " ");
+			System.out.print (words[i] + " ");//print to the screen the array  
 		}
 			
 	}
