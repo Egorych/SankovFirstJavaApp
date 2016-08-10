d7682c719e3e0d6d2c49a005539014c7726d76a3
Egorych <mr.stavr@mail.ru>
2016-08-10 18:55:23 +0200

    Regex

--- SankovJavaApp/src/RegexHW/RegexHW.java
+++ SankovJavaApp/src/RegexHW/RegexHW.java
@@ -5,7 +5,7 @@ public class RegexHW {
 
 	public static void main(String[] args) {
 		String text = "0x7FFF <p id=p1> 0x7AAF <p id=p1> kkkk  khg gkhjg";//create the line on type String
-		text = text.replaceAll("<[^>]*>.","<p> ");//replace all symbols except those that in expression regex on an empty symbol ""
+		text = text.replaceAll("<[^>]*>.","<p> ");//replace all symbols except those that in expression regex to the an empty symbol "" <p.*?>
 		System.out.println(text);
 	}
 }
